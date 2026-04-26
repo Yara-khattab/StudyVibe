f3d2f5b571345e7d217eb8aaca013528e246be0b
Yara khattab <khatttabyara@gmail.com>
2026-04-26 04:09:47 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -122,3 +122,9 @@ function resetGame() {
   gameOver = false;
   clearInterval(countdown);
 }
+
+const closeIcon = document.getElementById('cancel');
+const alertBox = document.querySelector('.alert_finished_game');
+closeIcon.addEventListener('click', () => {
+    alertBox.style.display = 'none'; 
+});
