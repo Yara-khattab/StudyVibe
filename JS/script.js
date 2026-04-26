a68611aa35cf35f4d8629536f67fad02a943ce0b
yara-khattab <khatttabyara@gmail.com>
2026-04-26 03:45:11 +0300

    add alert game end

--- JS/script.js
+++ JS/script.js
@@ -122,3 +122,8 @@ function resetGame() {
   gameOver = false;
   clearInterval(countdown);
 }
+const closeIcon = document.getElementById('cancel');
+const alertBox = document.querySelector('.alert_finished_game');
+closeIcon.addEventListener('click', () => {
+    alertBox.style.display = 'none'; 
+});
